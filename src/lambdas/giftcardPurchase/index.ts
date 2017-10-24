8bc90b46bada06acbeacdfbcdb0a94567bfcc489
jeff <jeff@giftbit.com>
2017-10-24 16:06:15 -0700

    Project structure.

---
+++ src/lambdas/giftcardPurchase/index.ts
@@ -0,0 +1,28 @@
+import "babel-polyfill";
+import * as cassava from "cassava";
+import * as giftbitRoutes from "giftbit-cassava-routes";
+
+export const router = new cassava.Router();
+
+router.route(new cassava.routes.LoggingRoute());
+router.route(new giftbitRoutes.HealthCheckRoute("/v1/storage/healthCheck"));
+
+const authConfigPromise = giftbitRoutes.secureConfig.fetchFromS3ByEnvVar<any>("SECURE_CONFIG_BUCKET", "SECURE_CONFIG_KEY_JWT");
+const roleDefinitionsPromise = giftbitRoutes.secureConfig.fetchFromS3ByEnvVar<any>("SECURE_CONFIG_BUCKET", "SECURE_CONFIG_KEY_ROLE_DEFINITIONS");
+router.route(new giftbitRoutes.jwtauth.JwtAuthorizationRoute(authConfigPromise, roleDefinitionsPromise));
+
+router.route("/v1/purchase")
+    .method("GET")
+    .handler(async evt => {
+        const auth: giftbitRoutes.jwtauth.AuthorizationBadge = evt.meta["auth"];
+        auth.requireIds("giftbitUserId");
+
+        return {
+            body: {
+                domain: process.env["LIGHTRAIL_DOMAIN"]
+            }
+        };
+    });
+
+//noinspection JSUnusedGlobalSymbols
+export const handler = router.getLambdaHandler();
