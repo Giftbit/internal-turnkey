9405fdce37e0e8a5e371e5f6f20a4222722ef5ac
jeff <jeff@giftbit.com>
2017-10-25 09:05:51 -0700

    Add stripe lambda structure.

--- src/lambdas/stripe/index.ts
+++ src/lambdas/stripe/index.ts
@@ -11,7 +11,7 @@ const authConfigPromise = giftbitRoutes.secureConfig.fetchFromS3ByEnvVar<any>("S
 const roleDefinitionsPromise = giftbitRoutes.secureConfig.fetchFromS3ByEnvVar<any>("SECURE_CONFIG_BUCKET", "SECURE_CONFIG_KEY_ROLE_DEFINITIONS");
 router.route(new giftbitRoutes.jwtauth.JwtAuthorizationRoute(authConfigPromise, roleDefinitionsPromise));
 
-router.route("/v1/turnkey/purchaseGiftcard")
+router.route("/v1/turnkey/stripe")
     .method("GET")
     .handler(async evt => {
         const auth: giftbitRoutes.jwtauth.AuthorizationBadge = evt.meta["auth"];
