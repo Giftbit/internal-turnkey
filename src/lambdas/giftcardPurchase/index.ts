7846a12f475ca2fd49784b536ad3c04196e0a801
jeff <jeff@giftbit.com>
2017-10-25 15:51:47 -0700

    Set secure config env vars.

--- src/lambdas/giftcardPurchase/index.ts
+++ src/lambdas/giftcardPurchase/index.ts
@@ -5,7 +5,6 @@ import * as giftbitRoutes from "giftbit-cassava-routes";
 export const router = new cassava.Router();
 
 router.route(new cassava.routes.LoggingRoute());
-router.route(new giftbitRoutes.HealthCheckRoute("/v1/storage/healthCheck"));
 
 const authConfigPromise = giftbitRoutes.secureConfig.fetchFromS3ByEnvVar<any>("SECURE_CONFIG_BUCKET", "SECURE_CONFIG_KEY_JWT");
 const roleDefinitionsPromise = giftbitRoutes.secureConfig.fetchFromS3ByEnvVar<any>("SECURE_CONFIG_BUCKET", "SECURE_CONFIG_KEY_ROLE_DEFINITIONS");
